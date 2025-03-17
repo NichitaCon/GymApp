21edc91515de53e9d39f5e8461cfadfb549aaca0
Nichita <N00233189@iadt.ie>
2025-03-17 19:02:28 +0000

    auth redirection implimented

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -94,18 +94,3 @@ export default function Auth() {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: 'stretch',
-  },
-  mt20: {
-    marginTop: 20,
-  },
-})
