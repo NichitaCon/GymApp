321121979aea225e9722fd6efd281e503f4e0c51
Nichita <N00233189@iadt.ie>
2025-03-23 19:52:49 +0000

    routine delete functionality added

--- components/RoutineListItem.tsx
+++ components/RoutineListItem.tsx
@@ -1,23 +1,38 @@
 import { View, Text, Pressable, TouchableOpacity } from "react-native";
-import Feather from "@expo/vector-icons/Feather";
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
 import { supabase } from "~/utils/supabase";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faMugSaucer } from '@fortawesome/free-solid-svg-icons/faMugSaucer'
+import Entypo from "@expo/vector-icons/Entypo";
 
 export default function RoutineListItem({ routine }) {
+
+    const deleteRoutine = async () => {
+        const { error } = await supabase
+            .from("routines")
+            .delete()
+            .eq("routine_id", routine.routine_id);
+    }
+
+    console.log("routine item id = ", routine.routine_id);
     return (
         <Link href={`/routine/${routine.routine_id}`} asChild>
             <Pressable className="p-4 mb-3 border rounded-xl border-gray-200 bg-gray-100">
-                <View className="flex-row">
-                    <View className="flex-1 gap-1">
-                        <Text className="text-2xl" numberOfLines={2}>
-                            {routine.name}, {routine.routine_id}{" "}
-                            {/* Displaying routine name */}
-                        </Text>
-                        {/* <Text className="text-gray-700">{routine.description}</Text> Displaying routine description */}
-                    </View>
+                <View className="flex-row justify-between items-center">
+                    <Text className="text-2xl" numberOfLines={2}>
+                        {routine.name}, {routine.routine_id}{" "}
+                        {/* Displaying routine name */}
+                    </Text>
+                    <Pressable onPress={() => {
+                        deleteRoutine();
+                    }}>
+                        <Entypo
+                            name="trash"
+                            size={24}
+                            color="grey"
+                            className="justify-end"
+                        />
+                    </Pressable>
+                    {/* <Text className="text-gray-700">{routine.description}</Text> Displaying routine description */}
                 </View>
             </Pressable>
         </Link>
