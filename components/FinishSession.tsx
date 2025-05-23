ad6bcf55b26fe9804fc71ba15d3fd834c1f38636
NichitaCon <nichitacon04@gmail.com>
2025-05-23 23:02:01 +0100

    FIX: Integrate endRest functionality in FinishButton component

--- components/FinishSession.tsx
+++ components/FinishSession.tsx
@@ -1,8 +1,10 @@
 import { Button, Pressable, Text, View } from "react-native";
 import { useAuth } from "~/contexts/AuthProvider";
 import { useSessionStore } from "~/store/sessionStore";
+import { useRestStore } from "~/store/restStore";
 
 export function FinishButton() {
+    const endRest = useRestStore((s) => s.endRest);
     const endSession = useSessionStore((s) => s.endSession);
     const sessionId = useSessionStore((state) => state.sessionId);
     const { user } = useAuth();
@@ -16,6 +18,7 @@ export function FinishButton() {
                 <Pressable
                     onPress={() => {
                         endSession(user.id);
+                        endRest();
                     }}
                 >
                     <Text className="bg-blue-400 p-2 px-4 rounded-full text-center font-semibold text-xl">
